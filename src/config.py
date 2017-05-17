7cac2d3722085e3af1d4d34056c3fcf91986e1a2
Kevin Yu <thekevinyu@gmail.com>
2017-05-17 02:04:10 -0700

    Deleting old files

--- src/config.py
+++ src/config.py
@@ -3,7 +3,6 @@ import sys
 
 if len(sys.argv) > 1 and os.path.exists(sys.argv[1]):
     sys.path.append(os.path.dirname(os.path.expanduser(sys.argv[1])))
-    from opt import *
-else:
-    from config_template import *
+
+from opt import *
 
