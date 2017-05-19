c3ec4aa5980ce478c4f6e55e7b0e5114835c3c80
Kevin Yu <thekevinyu@gmail.com>
2017-05-18 18:48:37 -0700

    Add softmax to the rastering function over a dictionary

--- src/raster.py
+++ src/raster.py
@@ -3,19 +3,17 @@ import theano.tensor as T
 
 from position_encoding import L
 from utils.complex import ComplexTuple
+from utils import tensor_softmax
 
 
 D = ComplexTuple(T.fmatrix("D_real"), T.fmatrix("D_imag"))
 S = ComplexTuple(*T.fvectors("scene_real", "scene_imag"))
 
 
-belief = L.IFFT(S.dimshuffle(0, "x") * D.conj).real
-x, y, z = belief.shape
-belief = T.nnet.softmax(belief.reshape((x * y, z))).reshape((x, y, z))
 # render belief over dictionary components in a scene
 raster_scene = theano.function(
         inputs=[D.real, D.imag, S.real, S.imag],
-        outputs=belief,
+        outputs=tensor_softmax(L.IFFT(S.dimshuffle(0, "x") * D.conj).real),
         allow_input_downcast=True)
 
 
